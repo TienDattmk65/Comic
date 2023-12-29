14e95fd58ee81789c73780370808dea0846a94bb
dinhhuong2002 <dinhthihuongdung2002@gmail.com>
2023-12-29 23:43:55 +0700

    fix: banner error display

---
+++ pages/page/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import Header from '@/components/Header'
+import Banner  from '@/components/Banner'
+import CardItem from '@/components/CardItem'
+
+const Home = () => {
+
+
+    return(
+        <>
+            <div>
+                <Banner/>
+            </div>
+        </>
+    )
+}
+export default Home;
